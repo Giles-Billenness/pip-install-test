1e261c4e9a2407461f29696e172d221d70973c78
Giles Billenness <giles.billenness@blueyonder.co.uk>
2023-10-29 15:45:34 +0000

    remove redundant

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='pip-test',
       author_email='test@email',
       url='TBC',
       packages=['test_module','data_process'],
-      install_requires=['pandas','numpy','argparse','os'],
+      install_requires=['pandas','numpy'],
       entry_points={'console_scripts': ['pip-test-print = test_module.hello_world:hello_world_function',
                                         'pip-test-split = data_process.split:split_Data']}
      )s
